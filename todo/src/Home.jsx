25822c339a2c6f86b7bf4a8d12775453b1331fed
iaditi <aditipython@gmail.com>
2024-10-30 16:28:00 +0530

    added changes

--- todo/src/Home.jsx
+++ todo/src/Home.jsx
@@ -1,8 +1,13 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import Create from './Create'
 
 function Home(){
     const [todos,setTodos] = useState([])
+    useEffect(()=>{
+     axios.get('https://localhost:3001/get')
+     .then(result=>setTodos(result.data))
+     .catch(err=>console.log(err))
+    },[])
     return(
         <div>
             <h2>Todo List</h2>
