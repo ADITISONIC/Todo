82cf6219e044c6a07fb74144006fbcf879cbd521
iaditi <aditipython@gmail.com>
2024-10-30 17:42:02 +0530

    add

--- Server/Models/Todo.js
+++ Server/Models/Todo.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const TodoSchema = new mongoose.Schema({
-    task: { type: String, required: true }
+    task: { type: String, required: true },
+    done:{type:Boolean,default:false}
 }, { timestamps: true });
 
 const TodoModel = mongoose.model("Todo", TodoSchema);
